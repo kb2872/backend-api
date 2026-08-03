5bf71069c7cf146def78ee78a6b2b7418bff10e1
Henry <kb2872@gmail.com>
2026-08-03 13:07:26 +0200

    feat: add authentication router and login request schema

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from app.api.users import router as users_router
+from app.api.auth import router as auth_router
+
 
 app = FastAPI(
     title="HenryLab API",
@@ -24,6 +26,10 @@ def health():
 
 
 app.include_router(users_router)
+app.include_router(auth_router)
+
+
+
 
 
 
